221b87da5c8286ed047e025b7836f39fcb264817
Nick Waywood <nwaywood@au1.ibm.com>
2020-01-06 10:15:57 +1100

    Initial commit

---
+++ src/Main.elm
@@ -0,0 +1,239 @@
+module Main exposing (..)
+
+import Browser
+import Browser.Navigation as Nav
+import Element exposing (..)
+import Element.Input as Input
+import Html exposing (Html)
+import Http
+import Json.Decode exposing (Decoder, field, list, map2, string)
+import Router exposing (Route(..), fromUrl, routeParser)
+import Url
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { key : Nav.Key
+    , url : Url.Url
+    , route : Route
+    , articles : ArticleStatus (List Article)
+    }
+
+
+type ArticleStatus a
+    = Loading
+    | Failure Http.Error
+    | Success a
+
+
+type alias Article =
+    { title : String
+    , author : String
+    }
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model key url Router.NotFound Loading, getArticles )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | GotArticles (Result Http.Error (List Article))
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | route = fromUrl url }, Cmd.none )
+
+        GotArticles res ->
+            case res of
+                Ok articles ->
+                    ( { model | articles = Success articles }, Cmd.none )
+
+                Err e ->
+                    ( { model | articles = Failure e }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Browser.Document Msg
+view model =
+    case model.route of
+        Router.Home ->
+            viewHomePage model
+
+        Router.About ->
+            viewAboutPage model
+
+        Router.NotFound ->
+            viewNotFound model
+
+
+type alias Page msg =
+    { title : String
+    , body : List (Html msg)
+    }
+
+
+buildPage : String -> Element msg -> Page msg
+buildPage title body =
+    { title = title
+    , body = List.singleton <| layout [] body
+    }
+
+
+header : Element msg
+header =
+    column []
+        [ text "Header"
+        , viewLink "/home"
+        , viewLink "/about"
+        ]
+
+
+template : Element msg -> Element msg
+template content =
+    column []
+        [ header
+        , content
+        ]
+
+
+viewHomePage : Model -> Page msg
+viewHomePage model =
+    buildPage "Home Page"
+        (template
+            (case model.articles of
+                Success a ->
+                    showArticles a
+
+                Failure e ->
+                    text <| "Failed to load articles\n" ++ parseHttpError e
+
+                Loading ->
+                    text "Loading"
+            )
+        )
+
+
+parseHttpError : Http.Error -> String
+parseHttpError e =
+    case e of
+        Http.BadUrl s ->
+            "BadUrl: " ++ s
+
+        Http.Timeout ->
+            "Timeout error"
+
+        Http.NetworkError ->
+            "Network Error"
+
+        Http.BadStatus i ->
+            "Bad Status: " ++ String.fromInt i
+
+        Http.BadBody b ->
+            "Bad body: " ++ b
+
+
+showArticles : List Article -> Element msg
+showArticles articles =
+    column [] (List.map showArticle articles)
+
+
+showArticle : Article -> Element msg
+showArticle article =
+    column [] [ text article.title, text article.author ]
+
+
+viewAboutPage : Model -> Page msg
+viewAboutPage model =
+    buildPage "About"
+        (template
+            (text "About page")
+        )
+
+
+viewNotFound : Model -> Page msg
+viewNotFound model =
+    buildPage "Not Found"
+        (template
+            (text "Not found")
+        )
+
+
+viewLink : String -> Element msg
+viewLink path =
+    link []
+        { url = path
+        , label = text path
+        }
+
+
+
+-- HTTP
+
+
+getArticles : Cmd Msg
+getArticles =
+    Http.get
+        { url = "https://hn.algolia.com/api/v1/search_by_date?tags=story&hitsPerPage=50"
+        , expect = Http.expectJson GotArticles articlesDecoder
+        }
+
+
+articlesDecoder : Decoder (List Article)
+articlesDecoder =
+    field "hits" (Json.Decode.list articleDecoder)
+
+
+articleDecoder : Decoder Article
+articleDecoder =
+    map2 Article
+        (field "title" string)
+        (field "author" string)
---
+++ src/Router.elm
@@ -0,0 +1,31 @@
+module Router exposing (Route(..), fromUrl, routeParser)
+
+import Url
+import Url.Parser as Parser exposing (Parser, map, oneOf, s, top)
+
+
+
+-- Define your routes and your parser
+
+
+type Route
+    = Home
+    | About
+    | NotFound
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ map Home (s "home")
+        , map About (s "about")
+        ]
+
+
+
+-- Create a helper function that converts a url to a route using the parser
+
+
+fromUrl : Url.Url -> Route
+fromUrl url =
+    Maybe.withDefault NotFound (Parser.parse routeParser url)
