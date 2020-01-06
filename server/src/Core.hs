17397e74fc1b0faf2e5d30a011234c3950bcde46
Antony Targett <atargett@au1.ibm.com>
2020-01-06 15:25:46 +1100

    feat: Added middleware to serve the index.html from elm app.

--- server/src/Core.hs
+++ server/src/Core.hs
@@ -4,10 +4,12 @@ module Core
     ) where
     
 import Web.Scotty
+import Network.Wai.Middleware.Static (staticPolicy, addBase)
 
 import Data.Monoid (mconcat)
 
-run = scotty 3000 $
-    get "/:word" $ do
+run = scotty 3000 $ do 
+    middleware $ staticPolicy $ addBase "../client/index.html"
+    get "/api/:word" $ do
         beam <- param "word"
         html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
